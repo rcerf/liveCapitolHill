7d66f806d64e26f59d51b784bbba426f38e30ce4
Rick Cerf <rickcerf@gmail.com>
2013-10-31 12:33:07 -0700

    updating to query from sunlightfoundation

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var superagent = require('superagent');
 var consolidate = require('consolidate');
+var config = require(__dirname + "/config.js");
+
+if(config == undefined || config.apikey == undefined || config.apikey == ""){
+  throw Error("MUST CREATE YOUR OWN CONFIG FILE, LOOK AT CONFIG.TMP FOR AN EXAMPLE");
+}
 
 
 var app = express();
@@ -13,25 +18,30 @@ app.set('views', __dirname + '/views');
 //Set up static folder
 app.use(express.static(__dirname + '/public'));
 
-var user = 'azat_co';
-var story_slug = 'kazan';
+var legislator = 'A000014';
+var method = 'votes';
+var field = 'voter_ids.'
+
+//basepoint+this.collection+"/?apikey="+this.apikey+this.query;
+
 
 //Paste your values
-var api_key = "";
-var username = "";
-var _token = "";
+var api_key = config.apikey;
 
 app.get('/',function(req, res){
-  //Fetch elements from Storify API
-  superagent.get("http://api.storify.com/v1/stories/" + user + "/" + story_slug)
-    .query({api_key: api_key,
-      username: username,
-      _token: _token})
+  //Fetch elements from Sunlight API
+  superagent.get("http://congress.api.sunlightfoundation.com/" + method +"?" + field + legislator + "__exists=true")
+    .set('X-APIKey', api_key)
     .set({  Accept: 'application/json' })
-    .end(function(e, storifyResponse){
-      if (e) next(e);
-      //Render template with story object in response body     
-      return res.render('index',storifyResponse.body.content);      
+    .end(function(e, sunlightResponse){
+      console.log(sunlightResponse.headers);
+      if (e){
+        console.log('e');
+        next(e);
+      } 
+      //Render template with story object in response body
+      console.log(sunlightResponse.body);
+      return res.render('index',sunlightResponse.body);      
     })
 
 })
