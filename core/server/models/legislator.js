5b6c6237fc6843f47a0bb9aa7dc2392cb3ee6273
Rick Cerf <rickcerf@gmail.com>
2013-11-11 16:50:09 -0800

    updated the legislator relationship to bill

--- core/server/models/legislator.js
+++ core/server/models/legislator.js
@@ -1,9 +1,36 @@
+var _ = require('underscore');
+var uuid = require('node-uuid');
+var when = require('when');
+var nodefn = require('when/node/function');
+var Bill = require('./bill').Bill;
+var Vote = require('./vote').Vote;
+var Nomination = require('./nomination').Nomination;
+
 var liveBookshelf = require('./base');
 
 //Create legislator model
 var Legislator = liveBookshelf.Model.extend({
+
   tableName: 'legislators',
-  bills: function() {
+
+  permittedAttributes: [
+        'id', 'uuid', 'name', 'password', 'email', 'image', 'cover', 'bio', 'website', 'location',
+        'accessibility', 'status', 'language', 'meta_title', 'meta_description', 'last_login', 'created_at',
+        'created_by', 'updated_at', 'updated_by'
+    ],
+
+  creating: function() {
+    var self = this;
+
+    liveBookshelf.Model.prototype.creating.call(this);
+  },
+
+  saving: function() {
+    this.set('name', this.name, this);
+  }
+
+  //Relationships
+  bill: function() {
     return this.belongsToMany(Bill).through(Vote);
   },
   nomination: function() {
