49f18806558e012ce96b786b99c8b2ff99b10c58
Rick Cerf <rickcerf@gmail.com>
2013-11-18 18:08:54 -0800

    cleaned code, working on legislator listener

--- public/main.js
+++ public/main.js
@@ -4,55 +4,59 @@ $(function(){ // on ready
   var $footer = $('.footer-container');
   var $button = $('.btn-primary');
   var $form = $('.form-control');
-
-  // $(document).bind('DOMNodeInserted', function(event) {
-  //   $('.legislator').on('click', function (e) {
-  //     e.preventDefault();
-  //     var bioguide = $(this).data('bioguide');
-  //   }
-
-  //     );
-  // }); 
-
-  // var displayResults = function(url){
-  //   $.get( url, function( data ) {
-  //       $(".results").empty();
-  //       $(".results").append(data);
-  //   });
-  // };
-
+  var $modal = $('#myModal');
 
   var postZip = function(e){
     e.preventDefault();
     // window.username = $('option:selected');
       var entry = $('.form-control').val();
       var url = window.location.origin +'/' + entry;
-      console.log(entry);
-      $('#myModal').modal({
+      $modal.modal({
           keyboard: true,
           remote: url
       });
   };
-  
+
+  // var bindLegislator = function(){
+  //   $('.legislator-name').on('click', function (e) {
+  //     e.preventDefault();
+  //     console.log("clicked");
+  //     var bioguide = $(this).data('bioguide');
+  //     console.log(bioguide);
+  //     var url = window.location.origin + '/legislator/' + bioguide;
+  //     displayResults(url);
+  //   });
+  // };
+
+
+  // var displayResults = function(url){
+  // $.get( url, function( data ) {
+  //     $(".results").empty();
+  //     $(".results").append(data);
+  //   });
+  // };
+
   //initiated zip lookup on button click
   $button.on('click', postZip);
 
   //initiate zip lookup on enter
   $form.on('keyup', function(e){
+    e.preventDefault();
     if(e.which === 13){
       postZip(e);
     }
   });
  
   //removes data on modal upon close so new zip code can be entered
-  $('body').on('hidden.bs.modal', '.modal', function () {
+  $('body').on('hidden.bs.modal', '.modal', function (e) {
+    e.preventDefault();
     $(this).removeData('bs.modal');
   });
 
   //resizes results dynamically w/ screen size
-  $results.css({'height': (($(window).height()*.74))+'px'});
+  $results.css({'height': (($(window).height()*.68))+'px'});
   $(window).resize(function(){
-      $results.css({'height': (($(window).height()*.74))+'px'});
+      $results.css({'height': (($(window).height()*.68))+'px'});
   });
 
   //resizes footer dynamically with screen size
