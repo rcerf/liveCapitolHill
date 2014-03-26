381ce85d7b7efeb0f98747e34350ab5ff47ba476
rcerf <rickcerf@gmail.com>
2014-03-26 15:26:02 -0700

    changed to ssl

--- sunlight.js
+++ sunlight.js
@@ -1,7 +1,7 @@
 var superagent = require('superagent');
 
 module.exports.fetchActiveBills = function(params, cb) {
-  superagent.get("http://congress.api.sunlightfoundation.com/bills?history.active=true&order=last_action_at&per_page=50"
+  superagent.get("https://congress.api.sunlightfoundation.com/bills?history.active=true&order=last_action_at&per_page=50"
     + "&fields=chamber,official_title,sponsor_id,history.active_at,last_version,votes")
     .set('X-APIKey', params.api_key)
     .set({  Accept: 'application/json' })
@@ -14,7 +14,7 @@ module.exports.fetchActiveBills = function(params, cb) {
 };
 
 module.exports.fetchLegislatorVotes = function(params, cb) {
-  superagent.get("http://congress.api.sunlightfoundation.com//votes?voter_ids." + params.bioguide +
+  superagent.get("https://congress.api.sunlightfoundation.com//votes?voter_ids." + params.bioguide +
     "__exists=true&fields=voter_ids,question,required,result,voted_at&per_page=50")
     .set('X-APIKey', params.api_key)
     .set({  Accept: 'application/json' })
@@ -27,7 +27,7 @@ module.exports.fetchLegislatorVotes = function(params, cb) {
 };
 
 module.exports.fetchLegislators = function(params, cb) {
-  superagent.get("http://congress.api.sunlightfoundation.com/legislators" + params.method + params.zip +"&per_page=all")
+  superagent.get("https://congress.api.sunlightfoundation.com/legislators" + params.method + params.zip +"&per_page=all")
     .set('X-APIKey', params.api_key)
     .set({  Accept: 'application/json' })
     .end(function(e, sunlightResponse){
